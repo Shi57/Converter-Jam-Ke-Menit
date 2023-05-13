3917377ae4a0c9f399a9271798f05c91e43af593
Shi57 <86053409+Shi57@users.noreply.github.com>
2023-05-13 11:45:46 +0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,5 +1,9 @@
-x = int(input())
+from colorama import Fore, Style
+
+print(Fore.RED + Style.BRIGHT + "Konversi Jam ke Menit dengan Python" + Style.RESET_ALL)
+
+x = int(input("Masukkan Angka: "))
 
 y = x * 60 #1 Jam = 60 Menit
 
-print(y)
+print("Hasil nya:",y, "Menit")
